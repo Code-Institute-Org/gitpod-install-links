8338401c2490c9f44a03c531078c99ad04d773fc
Matt Rudge <mrudge@gmail.com>
2019-09-19 11:12:49 +0100

    Update init_tasks.sh

--- init_tasks.sh
+++ init_tasks.sh
@@ -7,10 +7,6 @@
 
 echo "Setting the greeting"
 sed -i "s/USER_NAME/$GITPOD_GIT_USER_NAME/g" ${GITPOD_REPO_ROOT}/README.md
-echo "Creating the ${C9_USER} user in MySQL"
-mysql -e "CREATE USER '${C9_USER}'@'%' IDENTIFIED BY '';"
-echo "Granting privileges"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${C9_USER}'@'%' WITH GRANT OPTION;"
 echo "Checking for pip upgrade"
 pip3 install --upgrade pip
 echo "Done"
